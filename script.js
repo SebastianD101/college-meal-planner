dc11c9052e7d7cd8e723773cf368be9632b11567
Sebastian <61755980+SebastianD101@users.noreply.github.com>
2021-12-25 01:18:01 -0500

    in progress code

---
+++ script.js
@@ -0,0 +1,17 @@
+function b1Changed() {
+    if (event.target.value == "Ford") {
+        document.getElementById("b1check1").style.display = "block";
+        document.getElementById("b1check2").style.display = "block";
+        document.getElementById("b1text1").style.display = "block";
+        document.getElementById("b1text2").style.display = "block";
+    }
+    if (event.target.value == "Earhart") {
+        
+    }
+    if (event.target.value == "Windsor") {
+        
+    }
+    if (event.target.value == "Other") {
+        
+    }
+}
